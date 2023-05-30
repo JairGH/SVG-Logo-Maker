2abe9c1fadcee3a163e30654de67f81a29c8bba8
Jair Gomez <jairherber505@gmail.com>
2023-05-29 17:19:38 -0700

    done

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# SVG-Logo-Maker
+
+# SVG Logo Creator Application
+This is a command-line application built with Node.js that allows users to generate SVG logos. The application uses the prompt package to collect user input and generate SVG logos based on the provided specifications.
+
+## Prerequisites
+Make sure you have Node.js and npm (Node Package Manager) installed on your system.
+
+## Installation
+Clone or download the repository to your local machine.
+Open a terminal or command prompt and navigate to the project directory.
+Run the following command to install the required dependencies:
+- npm install
+
+## Usage
+To start the SVG Logo Creator application, run the following command in your terminal or command prompt:
+- node index.js
+
+The application will prompt you to enter the specifications for your logo, such as the shape, colors, and text content.
+
+Follow the on-screen prompts to provide the necessary information for each specification. 
+
+## Customization
+If you wish to customize or extend the functionality of the SVG Logo Creator application, you can modify the index.js file. It contains the main logic for generating SVG logos and handling user input.
+
+Feel free to explore the code and make changes according to your requirements.
+
+## Acknowledgments
+This application was developed using the prompt package, which provides a simple way to collect user input from the command line.
--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-
 const inquirer = require("inquirer");
 
 const fs = require("fs");
 
-
 const { Triangle, Square, Circle } = require("./shapes");
 
+function writeToFile(fileName, answers) {
+  let svgString = "";
 
-function writeToFile(fileName, answers) 
+  svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
 
+  svgString += "<g>";
+
+  svgString += `${answers.shape}`;
+
+  let shapeChoice;
+  if (answers.shape === "Triangle") {
+    shapeChoice = new Triangle();
+    svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.shapeBackgroundColor}"/>`;
+  } else if (answers.shape === "Square") {
+    shapeChoice = new Square();
+    svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.shapeBackgroundColor}"/>`;
+  } else {
+    shapeChoice = new Circle();
+    svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
+  }
+
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
+
+  svgString += "</g>";
+
+  svgString += "</svg>";
 
   fs.writeFile(fileName, svgString, (err) => {
     err ? console.log(err) : console.log("Generated logo.svg");
@@ -18,28 +40,27 @@ function writeToFile(fileName, answers)
 function promptUser() {
   inquirer
     .prompt([
-      
       {
         type: "input",
         message:
           "What text would you like you logo to display? (Enter up to three characters)",
         name: "text",
       },
-      
+
       {
         type: "input",
         message:
           "Choose text color (Enter color keyword OR a hexadecimal number)",
         name: "textColor",
       },
-      
+
       {
         type: "list",
         message: "What shape would you like the logo to render?",
         choices: ["Triangle", "Square", "Circle"],
         name: "shape",
       },
-      
+
       {
         type: "input",
         message:
@@ -48,10 +69,10 @@ function promptUser() {
       },
     ])
     .then((answers) => {
-
+      {
         writeToFile("logo.svg", answers);
+      }
     });
 }
-
 
 promptUser();
